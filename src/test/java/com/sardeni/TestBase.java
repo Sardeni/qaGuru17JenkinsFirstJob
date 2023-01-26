051f921fe9a2ebe32ad1877336b90e6670793178
Sardeni <nordisnsk@gmail.com>
2023-01-26 18:08:14 +0400

    added attachments

--- src/test/java/com/sardeni/TestBase.java
+++ src/test/java/com/sardeni/TestBase.java
@@ -2,10 +2,12 @@ package com.sardeni;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.RegistrationPage;
 
 public class TestBase {
@@ -16,8 +18,12 @@ public class TestBase {
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
-
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
     }
     @BeforeEach
     void addListener() {
@@ -26,12 +32,10 @@ public class TestBase {
 
     @AfterEach
     void addAttachments() {
-        //screenshots
-        // page source
-        //consol logs
-        // video
-
-
-
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
+
